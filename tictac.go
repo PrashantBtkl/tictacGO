006d568994de6980306c944f593d7d9862c42176
prashant-btkl <prashantbhatkal2000@gmail.com>
2021-04-13 20:49:17 +0530

    made the code somewhat executeable ¯\_(ツ)_/¯

--- tictac.go
+++ tictac.go
@@ -4,89 +4,86 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"bufio"
+	"strings"
+	"strconv"
 )
 
 
 func main(){
 	board := [3][7]string{{"|"," ","|"," ","|"," ","|"},{"|"," ","|"," ","|"," ","|"},{"|"," ","|"," ","|"," ","|"}}
 	turn := 0
-	gameOver := false
+	gameOver,_ := checkWin(board)
 
-	/*
 	user_role := decidePlayer()
+	fmt.Println(user_role)
 
-	loop till (turn > 8) or (checkWin true){
+	for (turn < 9) || (gameOver != true){
 		showBoard(board)
-		checkWin(board, user_role)
+		checkWin(board)
 		
 		if turn % 2 == user_role {
 			board = playerCmd(board, user_role)
-		}esle {
+		}else {
 			board = botMove(board, user_role)
 		}
-
-
-
 		turn += 1
-
 	}	
-	
-*/
-
 }
 
 
 
 
-func playerCmd(board [3][7]string, user_role){
+func playerCmd(board [3][7]string, user_role int) [3][7]string{
 	/* takes the command from player
 		for eg. move 1,2
 				scoreboard
 				quit
 	*/
-	var inp string
-
+	in := bufio.NewReader(os.Stdin)
+	fmt.Println()
 	fmt.Printf("tictactoe>")
-	fmt.Scanf("%s", &inp)
+    inp, err := in.ReadString('\n')
+    if err != nil{
+        panic(err)
+    }
 
 	cmd := strings.Split(inp, " ")
+	fmt.Println(cmd[0])
+	fmt.Println(cmd[1])
+	points := cmd[1]
 
 	switch cmd[0]{
-
 	case "move":
-
-		move(cmd[1])
-
+		i,j := move(points, board, user_role)
+		board = playerMove(i,j, board, user_role)
 	case "quit":
-
 		quit()
-
 	default:
 		panic("Enter valid commands (move, quit, scoreboard):")
-
+		quit()
 	}
 
-
-	func move(points string){
-		/* evaluates the index of the board from user
-			eg:- if input is 1,2 our board indices would be 1,5
-		*/
-		bp := make(map[int]int)
-		bp[0],bp[1],bp[2] = 1,3,5
-
-		i := strconv.Atoi(string(points[0]))
-		j := strconv.Atoi(string(points[2]))
-
-		playerMove(i,bp[j], board, user_role)
-	}
-
-	func quit(){
-		fmt.Println("bye")
-		os.Exit(1)
-	}	
+	return board
 }
 
+func move(points string, board [3][7]string, user_role int) (int,int){
+	/* evaluates the index of the board from user
+		eg:- if input is 1,2 our board indices would be 1,5
+	*/
+	bp := make(map[int]int)
+	bp[0],bp[1],bp[2] = 1,3,5
 
+	i,err := strconv.Atoi(string(points[0]))
+	if err != nil {
+        panic(err)
+    }
+	j,err := strconv.Atoi(string(points[2]))
+	if err != nil {
+        panic(err)
+    }
+	return i,bp[j]
+}
 
 
 func playerMove(i int, j int, board [3][7]string, user_role int) [3][7]string{
@@ -95,13 +92,13 @@ func playerMove(i int, j int, board [3][7]string, user_role int) [3][7]string{
 	*/
 
   if board[i][j] != " " {
-    fmt.Println("Please pick an unoccupied space.")
-    playerCmd()
+    fmt.Println("invalid move, place already taken")
+    playerCmd(board, user_role)
   }
 
-  if i < 3 || j < 7 {
+  if i > 2 || j > 6 {
     fmt.Println("Please enter valid matrix index from 0,0 - 2,2")
-    playerCmd()
+    playerCmd(board, user_role)
   }
 
   if user_role == 0{
@@ -110,7 +107,6 @@ func playerMove(i int, j int, board [3][7]string, user_role int) [3][7]string{
     board[i][j] = "X"
   }
   return board
-
 }
 
 
@@ -122,31 +118,32 @@ func botMove(board [3][7]string, user_role int) [3][7]string{
 	*/
 	role := make(map[int]string)
 	role[0],role[1] = "O","X"
-	bot_role = math.Abs(user_role - 1)
+	bot_role := 1 ^ user_role
 	
 	i,j := rand_idx()
-	if board[i][j] != " "{
+	fmt.Println(i,j)
+	for board[i][j] != " "{
 		i,j = rand_idx()
-	}else{
+	}
+	if board[i][j] == " "{
 		board[i][j] = role[bot_role]
 	}
 
-	func rand_idx() (int,int) {
+	return board
+}
+
+func rand_idx() (int,int) {
 		i := rand.Intn(3)
 		j := rand.Intn(7)
 
 		return i,j
 	}
 
-}
-
-
-
 
 func showBoard(b [3][7]string){
 
 	/*Prints the tic-tac-toe board*/
-
+	 fmt.Println("\n")
 	for  i := 0; i < 3; i++ {
 		fmt.Println()
     	for j := 0; j < 7; j++ {
@@ -169,8 +166,7 @@ func decidePlayer() int{
 
 
 
-
-func checkWin(b [3][7]string, user_role int) int{
+func checkWin(b [3][7]string) (bool,int){
 
 	/*
 		Checks if the player or the computer won
@@ -189,11 +185,17 @@ func checkWin(b [3][7]string, user_role int) int{
   	res[7]= b[0][3] + b[1][3] + b[2][3]
 
   	for _, v := range res {
-    	if v == "OOO"{
-      		return 0
+    	if v == "OOO" {
+      		return true,0
     	} else if v == "XXX"{
-      		return 1
+      		return true,1
     }
   }
-  return false
+  return false,-1
+}
+
+
+func quit(){
+	fmt.Println("bye")
+	os.Exit(1)
 }
