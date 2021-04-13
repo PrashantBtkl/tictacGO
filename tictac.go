09052093703657f971f0f9a585d322499bba04c5
prashant-btkl <prashantbhatkal2000@gmail.com>
2021-04-13 17:11:14 +0530

    added playerCmd, playerMove

--- tictac.go
+++ tictac.go
@@ -3,28 +3,32 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"bufio"
 )
 
 
 func main(){
 	board := [3][7]string{{"|"," ","|"," ","|"," ","|"},{"|"," ","|"," ","|"," ","|"},{"|"," ","|"," ","|"," ","|"}}
-	player := make(map[int]string)
-	turn := 1
+	turn := 0
+	gameOver := false
 
 	/*
-	decidePlayer()
+	player_turn := decidePlayer()
 
 	loop till game over{
-		showBoard()
+		showBoard(board)
 
 		
-		if check_player_turn == true {
-			playerMove()
+		if turn % 2 == player_turn {
+			board = playerCmd(board)
 		}esle {
-			botMove()
+			board = botMove()
 		}
 
 		checkWin()
+
+		turn += 1
 
 	}	
 	
@@ -34,6 +38,104 @@ func main(){
 
 
 }
+
+
+
+
+func playerCmd(board [3][7]string){
+	/* takes the command from player
+		for eg. move 1,2
+				scoreboard
+				quit
+	*/
+	var inp string
+
+	fmt.Printf("tictactoe>")
+	fmt.Scanf("%s", &inp)
+
+	cmd := strings.Split(inp, " ")
+
+	switch cmd[0]{
+
+	case "move":
+
+		move(cmd[1])
+
+	case "quit":
+
+		quit()
+
+	default:
+		panic("Enter valid commands (move, quit, scoreboard):")
+
+	}
+
+
+	func move(points string){
+		/* evaluates the index of the board from user
+			eg:- if input is 1,2 our board indices would be 1,5
+		*/
+
+		bp := make(map[int]int)
+		bp[0],bp[1],bp[2] = 1,3,5
+
+		i := strconv.Atoi(string(points[0]))
+		j := strconv.Atoi(string(points[0]))
+
+		playerMove(i,bp[j], board)
+
+
+	}
+
+	func quit(){
+		fmt.Println("bye")
+		os.Exit(1)
+	}
+	
+
+
+}
+
+
+
+
+func playerMove(i int, j int, board [3][7]string, player_turn int) [3][7]string{
+	/*
+		takes input commands from playerCmd()
+	*/
+	// Check for occupied spaces
+  if board[i][j] != " " {
+    fmt.Println("Please pick an unoccupied space.")
+    playerCmd()
+  }
+
+  // Check for out-of-bounds
+  if i < 3 || j < 7 {
+    fmt.Println("Please enter valid matrix index from 0,0 - 2,2")
+    playerCmd()
+  }
+
+  if player_turn == 0{
+    board[i][j] = "O"
+  }else{
+    board[i][j] = "X"
+  }
+  return b
+
+}
+
+
+
+
+func botMove() [3][7]string{
+	/*
+		 Random valid moves made by computer
+	*/
+
+}
+
+
+
 
 func showBoard(b [3][7]string){
 
@@ -49,6 +151,8 @@ func showBoard(b [3][7]string){
 }
 
 
+
+
 func decidePlayer() int{
 	/*
 		decides random 'O' or 'X' role to the player
@@ -58,30 +162,6 @@ func decidePlayer() int{
 	player_turn := rand.Intn(2)
 
 	return player_turn
-}
-
-func playerCmd(){
-	/* takes the command from player
-		for eg. move 1,2
-				scoreboard
-				quit
-	*/
-}
-
-func playerMove(){
-	/*
-		takes input commands from playerCmd()
-	*/
-
-}
-
-
-
-func botMove(){
-	/*
-		 Random valid moves made by computer
-	*/
-
 }
 
 
@@ -107,12 +187,12 @@ func checkWin(b [3][7]string) int{
 
   	for _, v := range res {
     	if v == "OOO"{
-      		return 1
+      		return 0
     	} else if v == "XXX"{
-      		return 2
+      		return 1
     }
   }
-  return 0
+  return 
 
 
 
